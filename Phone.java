f0ddcfcea16d286358a7ebbe6210c0f0d908caf1
Luis Cevallos <lcevallo@gmail.com>
2017-06-05 14:39:11 -0500

    Empezando con  OCA meap pagina 12

---
+++ Phone.java
@@ -0,0 +1,18 @@
+
+class Phone {
+	String model;
+	String company;
+	
+	Phone(String model){
+		this.model;
+		}
+	double weight;
+	void makeCall(String number)
+		{
+			//code
+		}
+	void receiveCall()
+	{
+		//code
+	}
+}
---
+++ chapter01/certification/Course.java
@@ -0,0 +1,26 @@
+/**
+ * @(#)Course.java
+ *
+ *
+ * @author 
+ * @version 1.00 2017/6/5
+ */
+package chapter01.certification;
+//package com.exams;
+
+public class Course {
+
+
+public static void main(String[] args) {
+	String name =/* Harry */ "Paul";
+	System.out.println(name);
+	//error
+	
+	String name2="Shre /* ya*/	Paul";
+	System.out.println(name2);
+	
+}
+
+    
+    
+}
